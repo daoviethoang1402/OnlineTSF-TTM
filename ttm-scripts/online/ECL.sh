233fa0df60e704cd68bd755c630f52878f9664c2
daoviethoang1402 <daoviethoang1402@gmail.com>
2026-07-11 11:57:18 +0700

    Remove RevIN because unnecessary

--- ttm-scripts/online/ECL.sh
+++ ttm-scripts/online/ECL.sh
@@ -16,8 +16,8 @@ itr=1
 freq=h
 train_ratio=0.5
 test_ratio=0.45
-batch_size=64
-data=ETTh1
+batch_size=16
+data=ECL
 
 for seq_len in 512 1024 1536
 do
@@ -25,12 +25,12 @@ for pred_len in 96 192 336 720
 do
     filename=logs/online/TTM'_'Online'_'$data'_'$seq_len'_'$pred_len.log
 
-    python run.py --model TinyTimeMixer --normalization RevIN --decoder_mode mix_channel \
+    python run.py --model TinyTimeMixer  --decoder_mode mix_channel \
     --dataset $data --seq_len $seq_len --pred_len $pred_len --freq $freq --train_ratio $train_ratio --test_ratio $test_ratio \
     --batch_size $batch_size \
     --learning_rate $learning_rate --itr $itr >> $filename 2>&1
     
-    python run.py --model TinyTimeMixer --normalization RevIN --decoder_mode mix_channel \
+    python run.py --model TinyTimeMixer  --decoder_mode mix_channel \
     --dataset $data --seq_len $seq_len --pred_len $pred_len --freq $freq --train_ratio $train_ratio --test_ratio $test_ratio \
     --online_method Online --batch_size $batch_size \
     --online_learning_rate $online_learning_rate --itr $itr >> $filename 2>&1
--- ttm-scripts/online/Jiaolong.sh
+++ ttm-scripts/online/Jiaolong.sh
@@ -25,12 +25,12 @@ for pred_len in 96 192 336 720
 do
     filename=logs/online/TTM'_'Online'_'$data'_'$seq_len'_'$pred_len.log
 
-    python run.py --model TinyTimeMixer --normalization RevIN --decoder_mode mix_channel \
+    python run.py --model TinyTimeMixer  --decoder_mode mix_channel \
     --dataset $data --seq_len $seq_len --pred_len $pred_len --freq $freq --train_ratio $train_ratio --test_ratio $test_ratio \
     --batch_size $batch_size \
     --learning_rate $learning_rate --itr $itr >> $filename 2>&1
     
-    python run.py --model TinyTimeMixer --normalization RevIN --decoder_mode mix_channel \
+    python run.py --model TinyTimeMixer  --decoder_mode mix_channel \
     --dataset $data --seq_len $seq_len --pred_len $pred_len --freq $freq --train_ratio $train_ratio --test_ratio $test_ratio \
     --online_method Online --batch_size $batch_size \
     --online_learning_rate $online_learning_rate --itr $itr >> $filename 2>&1

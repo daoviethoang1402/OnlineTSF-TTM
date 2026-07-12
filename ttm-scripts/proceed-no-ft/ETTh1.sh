265a3646e97f2b9541fe6a44e5b9c5f30d7624f2
daoviethoang1402 <daoviethoang1402@gmail.com>
2026-07-12 14:55:44 +0700

    Update scripts

--- ttm-scripts/proceed-no-ft/ETTh1.sh
+++ ttm-scripts/proceed-no-ft/ETTh1.sh
@@ -26,7 +26,7 @@ for seq_len in 512 1024 1536
 do
 for pred_len in 96 192 336 720
 do
-    filename=logs/proceed/TTM'_'Proceed'_'$data'_'$seq_len'_'$pred_len.log
+    filename=logs/proceed/TTM'_'Proceed'_'$data'_'$seq_len'_'$pred_len'_'freezeonline.log
 
     python run.py --model TinyTimeMixer --freeze_online --decoder_mode common_channel \
     --dataset $data --seq_len $seq_len --pred_len $pred_len --freq h --train_ratio $train_ratio --test_ratio $test_ratio \
